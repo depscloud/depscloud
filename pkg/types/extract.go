54e5d8071454ec3bc0eebc5bc6ba9edcf6653fd9
Mya Pitzeruse <j.mya.pitz@gmail.com>
2019-05-27 09:22:20 -0500

    nobug: removed unused methods

--- pkg/types/extract.go
+++ pkg/types/extract.go
@@ -1,27 +1,10 @@
 package types
 
 import (
-	"crypto/sha256"
-	"encoding/json"
-
 	desapi "github.com/deps-cloud/des/api"
 	dtsapi "github.com/deps-cloud/dts/api"
 	"github.com/deps-cloud/dts/pkg/store"
 )
-
-func encodeJSON(i interface{}) []byte {
-	data, err := json.Marshal(i)
-	if err != nil {
-		panic(err)
-	}
-	return data
-}
-
-func sha(body []byte) []byte {
-	hash := sha256.New()
-	hash.Write(body)
-	return hash.Sum(nil)
-}
 
 // ExtractSource will convert the provided SourceInformation into it's
 // corresponding GraphItem.
