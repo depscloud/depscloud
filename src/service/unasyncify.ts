6a0b6e189719c6d3b1fd950f2fafc14fee686446
Mya Pitzeruse <j.mya.pitz@gmail.com>
2019-05-30 20:49:07 -0500

    api: fixed up api parameter style that broke convention and was causing issues with the match/extract implementation

--- src/service/unasyncify.ts
+++ src/service/unasyncify.ts
@@ -4,25 +4,32 @@ import AsyncDependencyExtractor from "./AsyncDependencyExtractor";
 
 const logger = getLogger();
 
+function toString(error: Error): string {
+    if (error.stack) {
+        return error.stack;
+    }
+    return `${error.name}: ${error.message}`;
+}
+
 export default function unasyncify(instance: AsyncDependencyExtractor): IDependencyExtractor {
     return {
         match(request, callback) {
-            logger.trace(`[service] match request: ${JSON.stringify(request)}`);
+            logger.debug(`[service] match request: ${JSON.stringify(request)}`);
 
             instance.match(request)
                 .then((response) => callback(null, response))
                 .catch((error) => {
-                    logger.error(`[service] match error: ${error}`);
+                    logger.error(`[service] match error: ${toString(error)}`);
                     callback(error, null);
                 });
         },
         extract(request, callback) {
-            logger.trace(`[service] extract request: ${JSON.stringify(request)}`);
+            logger.debug(`[service] extract request: ${JSON.stringify(request)}`);
 
             instance.extract(request)
                 .then((response) => callback(null, response))
                 .catch((error) => {
-                    logger.error(`[service] extract error: ${error}`);
+                    logger.error(`[service] extract error: ${toString(error)}`);
                     callback(error, null);
                 });
         },
