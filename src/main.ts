61971b3764b5e865418ed96930efe2915c7ed42d
Mya Pitzeruse <j.mya.pitz@gmail.com>
2019-07-22 06:51:49 -0500

    gh-21: added support for tls (#22)

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,16 @@ import DependencyExtractorImpl from "./service/DependencyExtractorImpl";
 import unasyncify from "./service/unasyncify";
 import program = require("caporal");
 import health = require("grpc-health-check/health");
+import fs = require("fs");
+
+const asyncFs = fs.promises;
 
 const logger = getLogger();
 
 program.name("extractor")
     .option("--port <port>", "The port to bind to.", program.INT)
+    .option("--tls-key <key>", "The path to the private key used for TLS", program.STRING)
+    .option("--tls-cert <cert>", "The path to the certificate used for TLS", program.STRING)
     .action(async (args: any, options: any) => {
         configure({
             appenders: {
@@ -41,7 +46,22 @@ program.name("extractor")
         server.addService(DependencyExtractor.service, unasyncify(impl));
         server.addService(health.service, healthcheck);
 
-        server.bind(`0.0.0.0:${port}`, ServerCredentials.createInsecure());
+        let credentials = ServerCredentials.createInsecure();
+        if (options.tlsKey && options.tlsCert) {
+            logger.info("[main] configuring tls");
+
+            const [ key, cert ] = await Promise.all([
+                asyncFs.readFile(options.tlsKey),
+                asyncFs.readFile(options.tlsCert),
+            ]);
+
+            credentials = ServerCredentials.createSsl(null, [ {
+                private_key: key,
+                cert_chain: cert,
+            } ], true);
+        }
+
+        server.bind(`0.0.0.0:${port}`, credentials);
         logger.info(`[main] starting gRPC on :${port}`);
         server.start();
     })
