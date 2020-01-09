e7fd24c7e00eeecd5505c295013e942f699c75d8
Mya Pitzeruse <j.mya.pitz@gmail.com>
2020-01-09 00:21:10 -0600

    made package silent

--- scripts/package.sh
+++ scripts/package.sh
@@ -4,8 +4,8 @@ readonly os=$(uname | tr '[:upper:]' '[:lower:]')
 
 trap "rm -rf helm.tar.gz ${os}-amd64" EXIT
 
-curl -o helm.tar.gz https://get.helm.sh/helm-v3.0.2-${os}-amd64.tar.gz
-tar zxvf helm.tar.gz
+curl -sSL -o helm.tar.gz https://get.helm.sh/helm-v3.0.2-${os}-amd64.tar.gz
+tar zxf helm.tar.gz
 
 for chart in $(ls -1 incubator/); do
   ./${os}-amd64/helm package incubator/${chart} -d incubator
