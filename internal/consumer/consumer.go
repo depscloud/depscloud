a5d862114f2e982fa70b68dd5deb3236c4b3ef62
Mya Pitzeruse <j.mya.pitz@gmail.com>
2020-01-08 22:25:13 -0600

    gh-13: updated indexer to use latest version of the discovery client (#14)
    * gh-13: updated indexer to use latest version of the discovery client
    
    * gh-13: updated consumer to use per account credentials with ssh
    
    * gh-13: ran fmt

--- internal/consumer/consumer.go
+++ internal/consumer/consumer.go
@@ -9,6 +9,7 @@ import (
 	"github.com/deps-cloud/api/v1alpha/extractor"
 	"github.com/deps-cloud/api/v1alpha/schema"
 	"github.com/deps-cloud/api/v1alpha/tracker"
+	"github.com/deps-cloud/discovery/pkg/remotes"
 
 	"github.com/sirupsen/logrus"
 
@@ -16,12 +17,14 @@ import (
 	"gopkg.in/src-d/go-git.v4"
 	"gopkg.in/src-d/go-git.v4/plumbing/cache"
 	"gopkg.in/src-d/go-git.v4/plumbing/transport"
+	"gopkg.in/src-d/go-git.v4/plumbing/transport/http"
+	"gopkg.in/src-d/go-git.v4/plumbing/transport/ssh"
 	"gopkg.in/src-d/go-git.v4/storage/filesystem"
 )
 
 // RepositoryConsumer represent the contract for consuming repositories
 type RepositoryConsumer interface {
-	Consume(repository string)
+	Consume(repository *remotes.Repository)
 }
 
 // NewConsumer creates a consumer process that is agnostic to the ingress channel.
@@ -45,7 +48,9 @@ type consumer struct {
 
 var _ RepositoryConsumer = &consumer{}
 
-func (c *consumer) Consume(repository string) {
+func (c *consumer) Consume(repository *remotes.Repository) {
+	repourl := repository.RepositoryURL
+
 	dir, err := ioutil.TempDir(os.TempDir(), "dis")
 	if err != nil {
 		logrus.Errorf("failed to create tempdir")
@@ -61,15 +66,48 @@ func (c *consumer) Consume(repository string) {
 
 	storage := filesystem.NewStorage(gitfs, cache.NewObjectLRUDefault())
 	options := &git.CloneOptions{
-		URL:   repository,
+		URL:   repourl,
 		Depth: 1,
 	}
 
-	if c.authMethod != nil {
+	if repository.Clone != nil {
+		if basic := repository.Clone.GetBasic(); basic != nil {
+			auth := &http.BasicAuth{
+				Username: basic.GetUsername(),
+			}
+
+			if password := basic.GetPassword(); password != nil {
+				auth.Password = password.Value
+			}
+
+			options.Auth = auth
+		} else if publicKey := repository.Clone.GetPublicKey(); publicKey != nil {
+			user := publicKey.GetUser()
+			if user == "" {
+				user = "git"
+			}
+
+			password := publicKey.GetPassword()
+
+			var keys *ssh.PublicKeys
+			if privateKeyPath := publicKey.GetPrivateKeyPath(); privateKeyPath != nil {
+				keys, err = ssh.NewPublicKeysFromFile(user, privateKeyPath.Value, password)
+			} else if privateKey := publicKey.GetPrivateKey(); privateKey != nil {
+				keys, err = ssh.NewPublicKeys(user, []byte(privateKey.GetValue()), password)
+			}
+
+			if keys == nil || err != nil {
+				logrus.Errorf("[%s] failed to get public keys for repository", repourl)
+				return
+			}
+
+			options.Auth = keys
+		}
+	} else if c.authMethod != nil {
 		options.Auth = c.authMethod
 	}
 
-	logrus.Infof("[%s] cloning repository", repository)
+	logrus.Infof("[%s] cloning repository", repourl)
 	_, err = git.Clone(storage, fs, options)
 
 	if err != nil {
@@ -77,7 +115,7 @@ func (c *consumer) Consume(repository string) {
 		return
 	}
 
-	logrus.Infof("[%s] walking file system", repository)
+	logrus.Infof("[%s] walking file system", repourl)
 	queue := []string{""}
 	paths := make([]string, 0)
 
@@ -106,7 +144,7 @@ func (c *consumer) Consume(repository string) {
 		queue = newQueue
 	}
 
-	logrus.Infof("[%s] matching dependency files", repository)
+	logrus.Infof("[%s] matching dependency files", repourl)
 	matchedResponse, err := c.desClient.Match(context.Background(), &extractor.MatchRequest{
 		Separator: string(filepath.Separator),
 		Paths:     paths,
@@ -129,32 +167,32 @@ func (c *consumer) Consume(repository string) {
 		fileContents[matched] = string(data)
 	}
 
-	logrus.Infof("[%s] extracting dependencies", repository)
+	logrus.Infof("[%s] extracting dependencies", repourl)
 	extractResponse, err := c.desClient.Extract(context.Background(), &extractor.ExtractRequest{
-		Url:          repository,
+		Url:          repourl,
 		Separator:    string(filepath.Separator),
 		FileContents: fileContents,
 	})
 
 	if err != nil {
-		logrus.Errorf("failed to extract deps from repo: %s", repository)
+		logrus.Errorf("failed to extract deps from repo: %s", repourl)
 		return
 	}
 
-	logrus.Infof("[%s] storing dependencies", repository)
+	logrus.Infof("[%s] storing dependencies", repourl)
 	_, err = c.sourceService.Track(context.Background(), &tracker.SourceRequest{
 		Source: &schema.Source{
-			Url: repository,
+			Url: repourl,
 		},
 		ManagementFiles: extractResponse.GetManagementFiles(),
 	})
 
 	if err != nil {
-		logrus.Errorf("failed to update deps for repo: %s, %v", repository, err)
+		logrus.Errorf("failed to update deps for repo: %s, %v", repourl, err)
 		return
 	}
 
-	logrus.Infof("[%s] cleaning up file system", repository)
+	logrus.Infof("[%s] cleaning up file system", repourl)
 	if err := os.RemoveAll(dir); err != nil {
 		logrus.Errorf("failed to cleanup scratch directory: %s", err.Error())
 	}
