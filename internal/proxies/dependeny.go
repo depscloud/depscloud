dfb9a8dcc228058ba0a199ce5583640e672ef812
Mya Pitzeruse <j.mya.pitz@gmail.com>
2020-06-25 02:08:26 -0500

    gh-23: support grpc and http 1.1 on the ingress

---
+++ internal/proxies/dependeny.go
@@ -0,0 +1,27 @@
+package proxies
+
+import (
+	"context"
+
+	"github.com/deps-cloud/api/v1alpha/tracker"
+)
+
+func NewDependencyServiceProxy(client tracker.DependencyServiceClient) tracker.DependencyServiceServer {
+	return &dependencyService{
+		client: client,
+	}
+}
+
+type dependencyService struct {
+	client tracker.DependencyServiceClient
+}
+
+func (d *dependencyService) ListDependents(ctx context.Context, request *tracker.DependencyRequest) (*tracker.ListDependentsResponse, error) {
+	return d.client.ListDependents(ctx, request)
+}
+
+func (d *dependencyService) ListDependencies(ctx context.Context, request *tracker.DependencyRequest) (*tracker.ListDependenciesResponse, error) {
+	return d.client.ListDependencies(ctx, request)
+}
+
+var _ tracker.DependencyServiceServer = &dependencyService{}
