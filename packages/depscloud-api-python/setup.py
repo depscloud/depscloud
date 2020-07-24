6df488c086b69650ad5eb68ff13eec0d23286ddc
Mya Pitzeruse <j.mya.pitz@gmail.com>
2020-07-23 22:23:52 -0500

    v0.1.10

--- packages/depscloud-api-python/setup.py
+++ packages/depscloud-api-python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='depscloud_api',
-    version='0.1.9',
+    version='0.1.10',
     license='MIT',
     description='Python client to the deps.cloud API',
     author='deps.cloud Authors',
