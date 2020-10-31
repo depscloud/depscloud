4940822f1915e942725189da321dcb234f8dacb5
Mya Pitzeruse <j.mya.pitz@gmail.com>
2020-10-31 12:31:44 -0500

    fix vulnerable packages and updated v1beta kind field

--- tracker/internal/graphstore/v1beta/graphitem.go
+++ tracker/internal/graphstore/v1beta/graphitem.go
@@ -27,7 +27,7 @@ type GraphData struct {
 	K1             string        `json:"k1"           db:"k1"            gorm:"column:k1;type:varchar(64);primaryKey;index:secondary,priority:2"`
 	K2             string        `json:"k2"           db:"k2"            gorm:"column:k2;type:varchar(64);primaryKey;index:secondary,priority:1"`
 	K3             string        `json:"k3"           db:"k3"            gorm:"column:k3;type:varchar(64);primaryKey;index:secondary,priority:3"`
-	Kind           string        `json:"kind"         db:"kind"          gorm:"column:kind;type:varchar(55);index:kind"`
+	Kind           string        `json:"kind"         db:"kind"          gorm:"column:kind;type:varchar(255);index:kind"`
 	Encoding       Encoding      `json:"encoding"     db:"encoding"      gorm:"column:encoding"`
 	Data           string        `json:"data"         db:"data"          gorm:"column:data;type:text"`
 	DateDeleted    *sql.NullTime `json:"dateDeleted"  db:"date_deleted"  gorm:"column:date_deleted;index:date_deleted"`
